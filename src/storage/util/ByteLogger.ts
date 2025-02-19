3238cde576f207599bf6e91759ff778af552145c
Valentine Efagene <efagenevalentine@gmail.com>
2025-02-19 02:55:01 +0100

    feat: use nest logger in byte logger

--- src/storage/util/ByteLogger.ts
+++ src/storage/util/ByteLogger.ts
@@ -1,11 +1,13 @@
-import { Transform, TransformCallback } from 'node:stream'
+import { Logger } from "@nestjs/common";
+import { Transform, TransformCallback } from "node:stream";
 
 export class ByteLogger extends Transform {
+    private readonly logger: Logger = new Logger(ByteLogger.name);
     private bytesTransferred = 0;
 
     _transform(chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) {
         this.bytesTransferred += chunk.length;
-        console.log(`Transferred ${chunk.length} bytes. Total: ${this.bytesTransferred} bytes.`);
+        this.logger.log(`Transferred ${chunk.length} bytes. Total: ${this.bytesTransferred} bytes.`);
         this.push(chunk); // Pass data along
         callback();
     }
